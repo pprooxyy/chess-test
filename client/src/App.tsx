aa92393383c63d9cfef8655f76b1f77ef90a7487
Dmytro Malofieiev <maloy34522@gmail.com>
2023-05-16 11:31:26 +0200

    history initial

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import Navbar from "./Components/Navbar/Navbar";
+import History from "./Pages/History";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <History />
     </div>
   );
 }
