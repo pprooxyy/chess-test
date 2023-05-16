338f52c9366594cfd652f4c94f7cd7eb61744e13
pprooxyy <scvortsovmaxim@gmail.com>
2023-05-16 14:36:16 +0300

    routes

--- client/src/Pages/Friends/Friends.tsx
+++ client/src/Pages/Friends/Friends.tsx
@@ -1,37 +1,38 @@
-import React, { useState } from 'react';
-import "./Friends.css"
+import React, { useState } from "react";
+import "./Friends.css";
 
-function Friend() {
-    // const [friendName1, setFriendName] = useState('Pick');
-    // const [friendName2, setFriend] = useState('Nick');
-    // const [battles, setBattles] = useState([]);
-    // const [selectedGame, setSelectedGame] = useState(null);
+function Friends() {
+  // const [friendName1, setFriendName] = useState('Pick');
+  // const [friendName2, setFriend] = useState('Nick');
+  // const [battles, setBattles] = useState([]);
+  // const [selectedGame, setSelectedGame] = useState(null);
 
-    // const handleLookGame = (gameId) => {
-    //     // Logic to look at the selected game
-    //     console.log('Looking at game with ID:', gameId);
-    //     setSelectedGame(gameId);
-    // };
+  // const handleLookGame = (gameId) => {
+  //     // Logic to look at the selected game
+  //     console.log('Looking at game with ID:', gameId);
+  //     setSelectedGame(gameId);
+  // };
 
-    // const handleSendInvite = () => {
-    //     // Logic to send an invite to the friend
-    //     console.log('Invite sent to friend:', friendName);
-    // };
+  // const handleSendInvite = () => {
+  //     // Logic to send an invite to the friend
+  //     console.log('Invite sent to friend:', friendName);
+  // };
 
-    // const handleGenerateLink = () => {
-    //     // Logic to generate an invite link
-    //     console.log('Invite link generated for friend:', friendName);
-    // };
+  // const handleGenerateLink = () => {
+  //     // Logic to generate an invite link
+  //     console.log('Invite link generated for friend:', friendName);
+  // };
 
-    return (
-        <div className='mainContainer'>
-            <h1 className='titleFriends'>Friends</h1>
-            <div className='mainFriendContainer'>
-                <h3 className='onlineFriends'>Игры онлайн</h3>
-                <div>
-                    <ul className='flexLook'>
-                        {/* <h2>{friendName1} vs {friendName2}</h2> */} <div className='FriendVsFriend'>Nick vs Petiya</div>
-                        {/* {battles.map((battle) => (
+  return (
+    <div className="mainContainer">
+      <h1 className="titleFriends">Friends</h1>
+      <div className="mainFriendContainer">
+        <h3 className="onlineFriends">Игры онлайн</h3>
+        <div>
+          <ul className="flexLook">
+            {/* <h2>{friendName1} vs {friendName2}</h2> */}{" "}
+            <div className="FriendVsFriend">Nick vs Petiya</div>
+            {/* {battles.map((battle) => (
                         <>
                             <li key={battle.id}>
                                 {battle.startTime} - {battle.endTime}: {battle.status}
@@ -39,22 +40,19 @@ function Friend() {
                             </li>
                         </>
                     ))} */}
-                        <button>Look this game</button>
-                    </ul>
-                </div>
-                <h3>Отправить вызов другу</h3>
-                <div className='flexLook'>
-                    {/* <h2>{friendName1}</h2> */}
-                    <div className='FriendVsFriend'>Nick</div>
-                    <button>Send Invite</button>
-                    <button>Generate Invite Link</button>
-                </div>
-            </div>
+            <button>Look this game</button>
+          </ul>
         </div>
-
-
-    );
+        <h3>Отправить вызов другу</h3>
+        <div className="flexLook">
+          {/* <h2>{friendName1}</h2> */}
+          <div className="FriendVsFriend">Nick</div>
+          <button>Send Invite</button>
+          <button>Generate Invite Link</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Friend;
-
+export default Friends;
